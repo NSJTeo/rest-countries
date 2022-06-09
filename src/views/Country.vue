d380ea2431250809f9345b1b81c9df49e5b5c78e
nsjteo <n.teomacdonald@gmail.com>
2022-06-09 01:53:41 -0700

    Fetch info for individual countries

--- src/views/Country.vue
+++ src/views/Country.vue
@@ -1,12 +1,36 @@
 <template>
-  <main>Country</main>
+  <main>
+    <router-link to="/">Back</router-link>
+    <div v-if="country">
+      <p>{{ country.name.common }}</p>
+    </div>
+  </main>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'Country',
-  mounted() {
-    console.log(this.$route);
+  data() {
+    return {
+      country: null,
+    };
+  },
+  async mounted() {
+    console.log(this.$route.params.id);
+    const name = this.$route.params.id;
+    const { data } = await axios.get(
+      `https://restcountries.com/v3.1/name/${name}`
+    );
+    const [country] = data;
+    console.log(country);
+    this.setCountry(country);
+  },
+  methods: {
+    setCountry(country) {
+      this.country = country;
+    },
   },
 };
 </script>
